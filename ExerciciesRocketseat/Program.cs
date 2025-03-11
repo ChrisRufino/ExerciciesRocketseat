a6342199115fb41534424537dc7bf7b5b48ad394
christian <christisael@gmail.com>
2025-03-10 22:22:25 -0300

    First Commit

---
+++ ExerciciesRocketseat/Program.cs
@@ -0,0 +1,10 @@
+﻿
+{
+    static void Main()
+    {
+        Console.Write("Digite seu nome: ");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine($"Olá, {nome}! Seja muito bem-vindo!");
+    }
+}
